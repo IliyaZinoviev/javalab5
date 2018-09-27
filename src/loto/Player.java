ecf7ea25874810463f81265eae58409f2b0e8da5
IliyaZinoviev <mrmar333@yandex.ru>
2018-09-27 21:13:47 +0300

    rm super

--- src/loto/Player.java
+++ src/loto/Player.java
@@ -1,14 +1,12 @@
 package loto;
 
 import javafx.scene.paint.Color;
-import java.util.ArrayList;
 
 public class Player implements Runnable {
     private Color color;
     private Game game;
 
     public Player(Color color, Game game) {
-        super();
         this.color = color;
         this.game = game;
     }
